e51f3a1e48790bd44e6831920989cb1edc583672
AbdulbariSakka <abdalbareesakka2000@gmail.com>
2021-12-12 18:07:23 +0300

    form page edited

--- Frontend/imuSayahat/src/pages/NewPlace.vue
+++ Frontend/imuSayahat/src/pages/NewPlace.vue
@@ -11,7 +11,7 @@
                     <div class="row q-gutter-md">
                         <q-input
                             class="col-12 col-md-5"
-                            v-model="placeName"
+                            v-model="PlaceName"
                             type="text"
                             label="name *"
                             maxlength="50"
@@ -20,15 +20,15 @@
                         />
                         <q-input
                             class="col-12 col-md-5"
-                            v-model="phoneNumber"
+                            v-model="PhoneNumber"
                             label="Phone number *"
-                            type="number"
+                            type="text"
                             lazy-rules
                             :rules="[ val => val && val.length > 0 || 'Please type place phone number']"
                         />
                         <q-input
                             class="col-12 col-md-5"
-                            v-model="placeEmail"
+                            v-model="PlaceEmail"
                             label="Email *"
                             type="email"
                             lazy-rules
@@ -36,8 +36,8 @@
                         />
                         <q-input
                             class="col-12 col-md-5"
-                            v-model="entranceFee"
-                            type="number"
+                            v-model="EntranceFee"
+                            type="text"
                             label="Entrance fee *"
                             maxlength="50"
                             lazy-rules
@@ -45,25 +45,25 @@
                         />
                         <q-input
                             class="col-12 col-md-5"
-                            v-model="openingTime"
-                            type="datetime"
+                            v-model="OpeningTime"
+                            type="text"
                             label="opening time"
                             lazy-rules
                             :rules="[ val => val && val.length > 0 || 'Please type place oppening time']"
                         />
                         <q-input
                             class="col-12 col-md-5"
-                            v-model="closingTime"
+                            v-model="ClosingTime"
                             label="closing time"
-                            type="datetime"
+                            type="text"
                             lazy-rules
                             :rules="[ val => val && val.length > 0 || 'Please type place closing time']"
                         />
-                        <q-select class="col-11" standout="bg-teal text-white" v-model="PlaceCity" :options="options" label="City" />
+                        <q-select class="col-11" standout="bg-teal text-white" v-model="CityName" :options="cities" label="City" />
 
                         <q-input
                             class="col-11"
-                            v-model="placeAdress"
+                            v-model="PlaceAdress"
                             type="textarea"
                             label="Adress *"
                             lazy-rules
@@ -76,7 +76,7 @@
                     <div class="row q-gutter-md">
                         <q-input
                             class="col-12"
-                            v-model="imageUrl"
+                            v-model="ImageURL"
                             type="text"
                             label="Image URL"
                             lazy-rules
@@ -84,79 +84,83 @@
                         />
                         <q-input
                             class="col-12"
-                            v-model="placeDescription"
+                            v-model="PlaceDescription"
                             type="textarea"
                             label="Description *"
                             lazy-rules
-                            maxlength="100"
                             :rules="[ val => val && val.length > 0 || 'Please type place description']"
                         />
                     </div>
                 </div>
             </div>
             <div class="q-mx-lg">
-                <q-btn label="Submit" type="submit" color="green" />
+                <q-btn label="Submit" type="submit" color="green" class="q-mr-md" />
+                <q-btn label="Back" to="/" color="teal" />
             </div>
             </q-form>
-
         </div>
     </q-page>
 </template>
 
 <script>
 import { useQuasar } from 'quasar'
-import { ref } from 'vue'
+import { ref , computed} from 'vue'
 import axios from 'axios'
+import { useStore } from 'vuex'
 
 export default {
     setup() {
-        const $q = useQuasar()
+        const $q = useQuasar();
+        const store = useStore();
 
-        const placeName = ref(null);
-        const entranceFee = ref(null);
-        const imageUrl = ref(null);
-        const placeEmail = ref(null);
-        const placeDescription = ref(null);
-        const phoneNumber = ref(null);
-        const PlaceCity = ref(null);
-        const openingTime = ref(null);
-        const closingTime = ref(null);
-        const placeAdress = ref(null);
+        const PlaceName = ref('');
+        const EntranceFee = ref(0);
+        const ImageURL = ref('');
+        const PlaceEmail = ref('');
+        const PlaceDescription = ref('');
+        const PhoneNumber = ref('');
+        const CityName = ref('');
+        const OpeningTime = ref('');
+        const ClosingTime = ref('');
+        const PlaceAdress = ref('');
         
         const onSubmit = () => {
             axios({
                 url:"http://localhost:4000/api/place/post",
                 method: 'post', 
-                headers: {},
+                headers: {"Content-Type" : "application/json"},
                 data:{
-                    placeName,
-                    entranceFee,
-                    imageUrl,
-                    placeEmail,
-                    placeDescription,
-                    phoneNumber,
-                    placeAdress,
-                    openingTime,
-                    closingTime,
-                    PlaceCity
+                    "PlaceName": PlaceName.value,
+                    "CityName": CityName.value,
+                    "PlaceAdress": PlaceAdress.value,
+                    "PhoneNumber": PhoneNumber.value,
+                    "PlaceEmail": PlaceEmail.value,
+                    "OpeningTime": OpeningTime.value,
+                    "ClosingTime": ClosingTime.value,
+                    "EntranceFee": parseFloat(EntranceFee.value),
+                    "PlaceDescription": PlaceDescription.value,
+                    "ImageURL": ImageURL.value
                 }
             });
+
         };
+        
+        const cities = computed(() => {
+        return store.state.cities.cities;
+        })
 
         return{
-            placeName,
-            entranceFee,
-            imageUrl,
-            placeEmail,
-            placeDescription,
-            phoneNumber,
-            placeAdress,
-            openingTime,
-            closingTime,
-            PlaceCity,
-            options: [
-                'Google', 'Facebook', 'Twitter', 'Apple', 'Oracle'
-            ],
+            PlaceName,
+            EntranceFee,
+            ImageURL,
+            PlaceEmail,
+            PlaceDescription,
+            PhoneNumber,
+            PlaceAdress,
+            OpeningTime,
+            ClosingTime,
+            CityName,
+            cities,
             onSubmit,
         }
     },
